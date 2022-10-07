5c4c135ac5c76c07b6bda8319588eed56ed0a91a
DanielleM <dlmedeiro@gmail.com>
2022-10-07 16:35:55 -0700

    update landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ColorList from "./ColorList.js";
 import Color from "./Color.js";
 
@@ -11,8 +11,10 @@ function App() {
       </nav> */}
       <div className="test">
         <Routes>
-          <Route path="/" element={<ColorList />}></Route>
+          <Route path="/colors" element={<ColorList />}></Route>
           <Route path="/colors/:color" element={<Color />}></Route>
+          <Route path="/" element={<Navigate to="/colors" replace />}></Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </div>
