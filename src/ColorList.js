68cf85e5a409aae5a7e5f587232b485127c15e8a
DanielleM <dlmedeiro@gmail.com>
2022-10-07 12:39:06 -0700

    save colors

--- src/ColorList.js
+++ src/ColorList.js
@@ -5,31 +5,69 @@ import "./ColorList.css";
 function ColorList() {
   const [colorString, setColorString] = useState("000000");
   const [color, setColor] = useState("#000000");
+  const [text, setText] = useState("#000000");
+  const [savedColors, setSavedColors] = useState({});
 
-  const submit = (e) => {
+  const submitColor = (e) => {
     e.preventDefault();
   };
+  const submitText = (e) => {
+    e.preventDefault();
+    setText(e.target.value);
+  };
 
-  const handleChange = (e) => {
+  const handleColorChange = (e) => {
     let target = e.target.value;
     setColorString(target.substring(1));
     setColor(target);
-    console.log(color);
+    setText(target);
+  };
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  const saveColor = () => {
+    setSavedColors((savedColors) => ({ ...savedColors, [text]: color }));
+    setText("");
+    console.log(savedColors);
   };
 
   return (
     <div className="homePage">
-      <h1>This is the HomePage</h1>
+      <h1>Welcome to the Color Factory</h1>
+      <h2>Saved Colors</h2>
+      {Object.keys(savedColors).map((key, index) => {
+        return (
+          <div
+            key={index}
+            style={{ backgroundColor: `${color}` }}
+            className="savedColor"
+          >
+            <Link to={`/colors/${colorString}`}>{key}</Link>
+          </div>
+        );
+      })}
       <h3>Your chosen color is: {color}</h3>
-      <form onSubmit={submit}>
+      <form onSubmit={submitColor}>
         <input
           type="color"
           name="color"
           value={color}
-          onChange={handleChange}
+          onChange={handleColorChange}
         ></input>
       </form>
       <Link to={`/colors/${colorString}`}>Magic</Link>
+      <h3>Save Color</h3>
+      <div style={{ backgroundColor: `${color}` }} className="savedColor">
+        <form onSubmit={submitText}>
+          <input
+            type="text"
+            name="text"
+            value={text}
+            onChange={handleTextChange}
+          ></input>
+          <button onClick={saveColor}>Save</button>
+        </form>
+      </div>
     </div>
   );
 }
