5099018b91ad2f9f7551e0808e9900d7b91a5138
DanielleM <dlmedeiro@gmail.com>
2022-10-12 16:03:18 -0700

    redirect on incorrect color closes #1

--- src/Color.js
+++ src/Color.js
@@ -1,57 +1,40 @@
 import React, { useEffect, useState } from "react";
 import "./Color.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 // import ColorList from "./ColorList";
 
 function Color() {
+  const navigate = useNavigate();
   const { color } = useParams();
-  const [goHome, setGoHome] = useState(true);
+  const [goHome, setGoHome] = useState(false);
 
   useEffect(() => {
-    console.log(goHome);
-    console.log(color.length);
-
-    // https://www.geeksforgeeks.org/check-if-a-given-string-is-a-valid-hexadecimal-color-code-or-not/
     if (color.length === 3 || color.length === 6) {
       setGoHome(false);
-      for (let i = 1; i < color.length; i++) {
-        if (
-          (color[i] >= "0" && color[i] <= 9) ||
-          (color[i] >= "a" && color[i] <= "f") ||
-          color[i] >= "A" ||
-          color[i] <= "F"
-        ) {
-          setGoHome(false);
-        }
-      }
+    } else if (color.length !== 3 || color.length !== 6) {
+      setGoHome(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (goHome) {
+      return navigate("/colors");
     }
   }, [goHome]);
+
   return (
-    <div className="paint">
-      {goHome ? (
-        <div className="paint">
-          <div className="colorText">
-            <h1>Umm...</h1>
-            <h3>That color doesn't exist</h3>
-            <h5>Thumbs Down</h5>
-            <Link to={"/"}>Home</Link>
-          </div>
-        </div>
-      ) : (
-        <div
-          style={{
-            backgroundColor: `#${color}`,
-          }}
-          className="color"
-        >
-          <div className="colorText">
-            <h1>Here is your color</h1>
-            <h3>Isn't it Pretty</h3>
-            <h5>Yay</h5>
-            <Link to={"/"}>Home</Link>
-          </div>
-        </div>
-      )}
+    <div
+      style={{
+        backgroundColor: `#${color}`,
+      }}
+      className="color"
+    >
+      <div className="colorText">
+        <h1>Here is your color</h1>
+        <h3>Isn't it Pretty</h3>
+        <h5>Yay</h5>
+        <Link to={"/"}>Home</Link>
+      </div>
     </div>
   );
 }
